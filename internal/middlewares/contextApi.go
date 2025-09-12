50f2c5534aacb74905edda9b2a532720106e9f85
pawannn <pawankalyan1614@gmail.com>
2025-09-12 13:35:20 +0530

    chore: added context api functionality

--- internal/middlewares/contextApi.go
+++ internal/middlewares/contextApi.go
@@ -1,1 +1,27 @@
 package middlewares
+
+import (
+	"context"
+	"fmt"
+)
+
+type ContextType string
+
+var ContextKey ContextType = "CashTrackContext"
+
+type ApiContext struct {
+	UserID string `json:"userID"`
+}
+
+func AttachContext(ctx context.Context, contextData ApiContext) context.Context {
+	c := context.WithValue(ctx, ContextKey, contextData)
+	return c
+}
+
+func ParseContext(ctx context.Context) (*ApiContext, error) {
+	contextData, ok := ctx.Value(ContextKey).(ApiContext)
+	if !ok {
+		return nil, fmt.Errorf("unable to get data from the given contect")
+	}
+	return &contextData, nil
+}
