d34f127263633eeb44188e41351bb7ce1b70533a
pawannn <pawankalyan1614@gmail.com>
2025-09-15 18:57:07 +0530

    chore: fixed non existing user in db

--- internal/app/user/getuser.go
+++ internal/app/user/getuser.go
@@ -1,6 +1,8 @@
 package user
 
 import (
+	"net/http"
+
 	"github.com/pawannn/cashtrack/internal/domain/models"
 	"github.com/pawannn/cashtrack/internal/utils"
 )
@@ -18,6 +20,13 @@ func (uA *UserApp) GetUserByID(id string) (*models.User, utils.CashTrackError) {
 	if err != utils.NoErr {
 		return nil, err
 	}
+	if user == nil {
+		return nil, utils.CashTrackError{
+			Code:    http.StatusNotFound,
+			Message: "User is not registered with cashtrack",
+			Error:   nil,
+		}
+	}
 
 	err = uA.cacheRepo.StoreUserInfo(user)
 	if err != utils.NoErr {
@@ -31,6 +40,13 @@ func (uA *UserApp) GetUserByPhone(phone string) (*models.User, utils.CashTrackEr
 	user, err := uA.databaseRepo.GetUserByPhone(phone)
 	if err != utils.NoErr {
 		return nil, err
+	}
+	if user == nil {
+		return nil, utils.CashTrackError{
+			Code:    http.StatusNotFound,
+			Message: "User is not registered with cashtrack",
+			Error:   nil,
+		}
 	}
 
 	return user, utils.NoErr
--- internal/utils/random.go
+++ internal/utils/random.go
@@ -5,7 +5,3 @@ import "github.com/google/uuid"
 func NewUUID() string {
 	return uuid.New().String()
 }
-
-func GenerateRandomInt() int {
-	return 0
-}
