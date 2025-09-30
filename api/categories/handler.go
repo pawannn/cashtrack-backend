d2c0738b219fdd048529b94f30b13706395476c3
pawannn <pawankalyan1614@gmail.com>
2025-10-01 00:45:52 +0530

    chore: added log statements

--- api/categories/handler.go
+++ api/categories/handler.go
@@ -10,10 +10,16 @@ import (
 
 func (cA *CategoriesApi) GetCategories(c *gin.Context) {
 	reqID := utils.NewUUID()
+
+	cA.categoriesLogger.Info(reqID, "Fetching all categories")
+
 	categories, err := cA.categoriesService.GetCategories()
 	if err != utils.NoErr {
+		cA.categoriesLogger.Error(reqID, "Failed to fetch categories", err.Error)
 		cashTrackHttp.SendResponse(c, reqID, err.Code, err.Message, err)
 		return
 	}
-	cashTrackHttp.SendResponse(c, reqID, http.StatusOK, "fetched categories successfully", categories)
+
+	cA.categoriesLogger.Info(reqID, "Fetched categories successfully", len(categories))
+	cashTrackHttp.SendResponse(c, reqID, http.StatusOK, "Fetched categories successfully", categories)
 }
